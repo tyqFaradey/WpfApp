6066d30ed25ed6a993e6c7cc09b6532cef8675a4
tyqFaradey <vladushka.potapov@gmail.com>
2025-09-30 19:17:30 +0700

    minor changes

--- Domain/Request.cs
+++ Domain/Request.cs
@@ -2,5 +2,15 @@ namespace Domain;
 
 public class Request
 {
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }
+    public string Model { get; set; }
+    public string Description { get; set; }
     
+    public string ClientFullName { get; set; }
+    public string ClientPhoneNumber { get; set; }
+    
+    public string PerformerFullName { get; set; } 
+    public string Status { get; set; } 
 }
